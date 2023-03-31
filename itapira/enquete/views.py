a30d337009619dbd2ae43e749d68c32f9ca6e91c
Luis F. Previ <luisfpre5@gmail.com>
2023-03-31 10:52:32 -0300

    .

--- itapira/enquete/views.py
+++ itapira/enquete/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index (resquest):
     return HttpResponse('seila, lukita')
+
+def flasco (resquest):
+    return HttpResponse('Uniao flasco, nova ordem mudial')
